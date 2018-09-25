87c73b6bd334a98675e5b11bd85134b361cf42f3
marrtys <marrtys@gmail.com>
2018-09-25 17:00:15 +0300

    createdApi

---
+++ src/main/java/org/marrtys/project/FormulaParser.java
@@ -0,0 +1,20 @@
+package org.marrtys.project;
+
+public class FormulaParser {
+
+    double pars(String text) {
+
+        text = text.trim();
+
+
+        String[] items = text.split(" ");
+
+        return 0;
+
+
+    }
+
+
+}
+
+
---
+++ src/main/java/org/marrtys/project/MainMet.java
@@ -0,0 +1,16 @@
+package org.marrtys.project;
+
+public class MainMet {
+
+    public static void main (String[] args) {
+
+        FormulaParser fpr = new FormulaParser();
+
+        double result = fpr.pars(" 5.6 + 5");
+
+        System.out.println("" + result);
+
+    }
+
+
+}
